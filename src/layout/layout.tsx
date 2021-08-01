634cdc2a38ea9ab4bb09c0da7e521f882527a9cf
Esmaeil Abedi <esmaeil.abedi@snapp.cab>
2021-08-01 22:05:58 +0430

    Revert "feat: add back to top button"
    This reverts commit 55d4b66d21c65e878d106e25752c2148760b16a4.

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,6 +1,4 @@
-import React, { ReactElement, ReactNode, useRef } from 'react';
-// Hardcode Text
-import findText from '../replace-hardcode-text';
+import React, { ReactElement, ReactNode } from 'react';
 // Styles
 import styles from './layout.module.css';
 
@@ -8,26 +6,8 @@ interface IProps {
   children: ReactNode;
 }
 
-const Layout: React.FC<IProps> = ({ children }: IProps): ReactElement => {
-  const topRefElement = useRef<HTMLDivElement>(null);
-  const handleClick = () => {
-    if (topRefElement.current) {
-      topRefElement.current.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
-  return (
-    <div className={styles.container}>
-      <div ref={topRefElement} />
-      {children}
-      <button
-        type='button'
-        onClick={handleClick}
-        className={styles.backToTopButton}
-      >
-        {findText('top')}
-      </button>
-    </div>
-  );
-};
+const Layout: React.FC<IProps> = ({ children }: IProps): ReactElement => (
+  <div className={styles.container}>{children}</div>
+);
 
 export default Layout;
--- src/replace-hardcode-text/en.ts
+++ src/replace-hardcode-text/en.ts
@@ -6,7 +6,6 @@ const texts: { [key: string]: string } = {
     'Which breed is more closer to your dog breed? - Please Select one',
   getInformation: 'Please wait... we are recieving informations',
   noMatch: 'There is no match for your photo. please select another one',
-  top: 'Top',
 };
 
 export default texts;
